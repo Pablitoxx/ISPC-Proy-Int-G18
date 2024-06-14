7a673e72f2ac2dbc8f693b1891244399d1b6f624
Pablitoxx <pablo.ocano@hotmail.com>
2024-06-14 01:00:17 -0300

    -

--- bdReservaRestaurante/Script_final_BD_Resto.sql
+++ bdReservaRestaurante/Script_final_BD_Resto.sql
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `reserva_rest`.`usuarios` (
   `contraseña` VARCHAR(45) NULL DEFAULT NULL,
   `Nombre` VARCHAR(45) NULL DEFAULT NULL,
   `Apellido` VARCHAR(45) NULL DEFAULT NULL,
-  `telefono` INT(11) NOT NULL,
+  `telefono` INT(20) NOT NULL,
   PRIMARY KEY (`usuario_id`),
   UNIQUE INDEX `email` (`email` ASC) ,
   UNIQUE INDEX `Nombre` (`Nombre` ASC) ,
@@ -94,16 +94,20 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4;
 
+
+ -- Inserción de datos en tabla Restaurante
 INSERT INTO restaurante (nombre, calle, numero, tipo_de_comida)
 VALUES  ('Bohemia Mexican', 'Av Pueyrredon', 1345, 'Mexicana'),
-	('Bohemia Chinese', 'Gral Paz', 72, 'China'),
+		('Bohemia Chinese', 'Gral Paz', 72, 'China'),
         ('Bohemia Arabian', 'Av 24 de Septiebre', 791, 'Arabe'),
         ('Bohemia Burguer', 'Obispo Trejo', 1200, 'Hamburguesa'),
         ('Bohemia Italian', 'Av Colon', 2840, 'Italiana'),
         ('Bohemia Indian', 'Rondeu', 439, 'India'),
         ('Bohemia Grill', 'Rafael Nuñez', 3248, 'Parrilla'),
         ('Bohemia Fish & Shellfish', 'Sucre', 2504, 'Pescado y Marisco');
-
+        
+        
+  -- Inserción de datos en tabla Mesa
 INSERT INTO mesa (capacidad, disponibilidad, restaurante_id)
 VALUES  (2, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1),
         (2, 1, 2), (4, 1, 2), (5, 1, 2), (6, 1, 2),
@@ -113,7 +117,9 @@ VALUES  (2, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1),
         (2, 1, 6), (4, 1, 6), (5, 1, 6), (6, 1, 6),
         (2, 1, 7), (4, 1, 7), (5, 1, 7), (6, 1, 7),
         (2, 1, 8), (4, 1, 8), (5, 1, 8), (6, 1, 8);
-
+        
+        
+ -- Inserción de datos en tabla Reserva 
 INSERT INTO reserva (reserva_id, fecha, hora, usuario_id, restaurante_id)
 VALUES  (1,'2024-06-13','08:00:00',1,2),
 (2,'2024-06-13','09:00:00',2,3),
@@ -124,11 +130,13 @@ VALUES  (1,'2024-06-13','08:00:00',1,2),
 (7,'2024-06-13','14:00:00',4,1),
 (8,'2024-06-13','15:00:00',1,3);
 
+
+ -- Inserción de datos en tabla Usuarios
 INSERT INTO `usuarios` (usuario_id, email, contraseña, Nombre, Apellido, Telefono)
-VALUES (1,'pabblloocanno@gmail.com','pablo1','Pablo','Ocaño',2147483648),
-(2,'soledad.caudana@gmail.com','sole1','Soledad','Caudana',2147483649),
-(3,'fernandopajon06@gmail.com','fer1','Fernando','Pajon',2147483640),
-(4,'nachobressanini1@gmail.com','nacho1','Ignacio ','Bressanini',2147483641);
+VALUES  (1,'pabblloocanno@gmail.com','pablo1','Pablo','Ocaño',2147483648),
+		(2,'soledad.caudana@gmail.com','sole1','Soledad','Caudana',2147483649),
+		(3,'fernandopajon06@gmail.com','fer1','Fernando','Pajon',2147483640),
+		(4,'nachobressanini1@gmail.com','nacho1','Ignacio ','Bressanini',2147483641);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
